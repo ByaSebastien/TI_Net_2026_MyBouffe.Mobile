0910baca8f4b2053fbcd71a0bcc9841bad372847
Bya Sebastien <byaebastien@hotmail.com>
2026-01-08 09:43:14 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TI_Net_2026_MyBouffe.Mobile',
+  webDir: 'www'
+};
+
+export default config;
